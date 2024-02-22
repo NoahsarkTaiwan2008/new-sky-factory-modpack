39547e55c80a4c01f6e8e5d43315e422dfc4157e
NoahsarkTaiwan <ben109092ben@gmail.com>
2024-02-22 18:00:29 +0800

    projecte配方更新

--- kubejs/server_scripts/mods/projecte.js
+++ kubejs/server_scripts/mods/projecte.js
@@ -1,4 +1,7 @@
 ServerEvents.recipes((event) => {
   event.remove({ output: "projecte:transmutation_table" });
   event.remove({ output: "projecte:philosophers_stone" });
+
+  event.remove({output: "projecte:rm_katar"})
+  event.remove({output: "projecte:rm_morning_star"})
 });
