c712bea112b88e7786d23fd6c721ad0728fe066b
NoahsarkTaiwan <ben109092ben@gmail.com>
2024-02-22 19:02:32 +0800

    新增垃圾堆的合成

---
+++ kubejs/server_scripts/mods/rats.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes((event) => {
+    event.shapeless("rats:garbage_pile", [
+        "minecraft:bone",
+        "minecraft:rotten_flesh",
+        "minecraft:dirt",
+        "minecraft:cobblestone",
+        "minecraft:egg",
+        "rats:cheese"
+    ])
+});
