9d3f586b10c4bf8a24ffb2f0b33961f1123a4325
NoahsarkTaiwan <ben109092ben@gmail.com>
2024-02-24 11:48:38 +0800

    projectE配方更新

--- kubejs/server_scripts/mods/projecte.js
+++ kubejs/server_scripts/mods/projecte.js
@@ -4,4 +4,9 @@ ServerEvents.recipes((event) => {
 
   event.remove({output: "projecte:rm_katar"})
   event.remove({output: "projecte:rm_morning_star"})
+
+  event.remove({output: "projecte:gem_helmet"})
+  event.remove({output: "projecte:gem_chestplate"})
+  event.remove({output: "projecte:gem_leggings"})
+  event.remove({output: "projecte:gem_boots"})
 });
